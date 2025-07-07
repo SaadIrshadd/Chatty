e9f80c139d7b8f148d7e5bb97687c3766f6c2328
SaadIrshadd <saad2005irshadd@gmail.com>
2025-07-07 21:52:36 +0500

    Added ProtectRoute middleware and updateProfile endpoint for authRoute

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,9 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
 
-export const signup = async (req,res) => {
+export const signup = async (req, res) => {
     const {fullName, email, password} = req.body
    
     try {
@@ -48,7 +49,7 @@ export const signup = async (req,res) => {
     }
 }
 
-export const login = async (req,res) => {
+export const login = async (req, res) => {
     const {email, password} = req.body;
    
     try {
@@ -77,7 +78,7 @@ export const login = async (req,res) => {
     }
 }
 
-export const logout = (req,res)=>{
+export const logout = (req, res) => {
     try {
         res.cookie("jwt", "", {maxAge:0});
         res.status(200).json({ message: "Logged Out Successfully" });
@@ -85,5 +86,25 @@ export const logout = (req,res)=>{
     } catch (error) {
         console.log("Error in lofout controller", error.message);
         res.status(500).json({ message: "Internal Server Error" });    
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id
+
+        if(!profilePic){
+            return res.status(400).json({ message: "Profile pic is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updateUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new:true});
+
+        res.status(200).json(updateUser);
+        
+    } catch (error) {
+        console.log("Error in update profile", error.message);
+        res.status(500).json({ messsage: "Internal Server Error" });
     }
 }
