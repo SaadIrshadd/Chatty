4bb31555b168f1092fc4a7f78a495fa8324e4d82
SaadIrshadd <saad2005irshadd@gmail.com>
2025-07-13 22:44:47 +0500

    Fixed a typo in route

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(
 
 //ROUTES
 app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/messages", messageRoutes)
 
 app.listen(PORT, ()=>{
     console.log("server is running on PORT: ", PORT);
