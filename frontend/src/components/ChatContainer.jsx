b20d72bbba6aae2f6ffc32e70c1b266dbb8d4317
SaadIrshadd <saad2005irshadd@gmail.com>
2025-07-14 00:58:33 +0500

    Added functionality to send messages and images

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -11,14 +11,15 @@ const ChatContainer = () => {
     getMessages(selectedUser._id)
   }, [selectedUser._id, getMessages])
 
-  if(isMessagesLoading) return (
-    <div className='flex-1 flex flex-col overflow-auto'>
-      <ChatHeader />
-      <MessageSkeleton />
-      <MessageInput />
-    </div>
+  if(isMessagesLoading){
+    return (
+      <div className='flex-1 flex flex-col overflow-auto'>
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
   )
-
+}
   
   return (
     <div className='flex-1 flex flex-col overflow-auto'>
