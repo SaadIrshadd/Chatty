2a0f3df4e86dab2dfaefc8e144b852740f9ce4ea
SaadIrshadd <saad2005irshadd@gmail.com>
2025-09-04 10:31:56 +0500

    production

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,10 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: [
+            "http://localhost:5173",
+            "https://chatty123.vercel.app"
+        ],
         credentials: true
     })
 );
